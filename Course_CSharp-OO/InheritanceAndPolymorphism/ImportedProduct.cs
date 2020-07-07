b96d42a8279a08694fcd1a909c0c68344a8da16b
Henrique Holtz <henrique_holtz@hotmail.com>
2020-07-07 07:01:49 -0300

    Initial commit from Git Bash

---
+++ Course_CSharp-OO/InheritanceAndPolymorphism/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndPolymorphism
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price,double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string priceTag()
+        {
+            return base.priceTag();
+        }
+        public double TotalPrice()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Course_CSharp-OO/InheritanceAndPolymorphism/UsedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceAndPolymorphism
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price,DateTime manufactureDate) :base(name,price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public override string priceTag()
+        {
+            return base.priceTag();
+        }
+    }
+}
